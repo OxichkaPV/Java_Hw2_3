ca396ea90fd710dd47b48590d3c596846ab4da41
oxanaP <oxichka@list.ru>
2024-02-04 18:19:04 +0700

    Changed Main and CreditPaymentService

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,10 +1,9 @@
 public class CreditPaymentService {
-    public int calculate(int pSumma, double pProcent, int pSrok) {
-        double result;
-        double mProcent = pProcent / (12 * 100);
-        int mSrok = pSrok * 12;
-        result = pSumma * (mProcent * (Math.pow(1 + mProcent, mSrok) / (Math.pow(1 + mProcent, mSrok) - 1)));
-        System.out.println("Срок кредита: " + pSrok + " год");
+    public int calculate(int sum, double percent, int term) {
+        double monthPercent = percent / (12 * 100);
+        int monthTerm = term * 12;
+        double result = sum * (monthPercent * (Math.pow(1 + monthPercent, monthTerm) / (Math.pow(1 + monthPercent, monthTerm) - 1)));
+        System.out.println("Срок кредита: " + term + " год");
         return (int) result;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,15 @@
 public class Main {
     public static void main(String[] args) {
         CreditPaymentService cpService = new CreditPaymentService();
-        int srokYear;
-        double procentYear = 9.9;
-        int summaKredita = 1_000_000;
-        System.out.println("Сумма кредита: " + summaKredita + " руб.");
-        System.out.println("Процент: " + procentYear + " %");
-        srokYear = 1; // 1 год
-        System.out.println("Ежемесячный платеж: " + cpService.calculate(summaKredita, procentYear, srokYear)+ " руб.");
-        srokYear = 2; // 2 года
-        System.out.println("Ежемесячный платеж: " + cpService.calculate(summaKredita, procentYear, srokYear)+ " руб.");
-        srokYear = 3; // 3 года
-        System.out.println("Ежемесячный платеж: " + cpService.calculate(summaKredita, procentYear, srokYear)+ " руб.");
+        double yearPercent = 9.9;
+        int credit = 1_000_000;
+        System.out.println("Сумма кредита: " + credit + " руб.");
+        System.out.println("Процент: " + yearPercent + " %");
+        int termYear = 1; // 1 год
+        System.out.println("Ежемесячный платеж: " + cpService.calculate(credit, yearPercent, termYear)+ " руб.");
+        termYear = 2; // 2 года
+        System.out.println("Ежемесячный платеж: " + cpService.calculate(credit, yearPercent, termYear)+ " руб.");
+        termYear = 3; // 3 года
+        System.out.println("Ежемесячный платеж: " + cpService.calculate(credit, yearPercent, termYear)+ " руб.");
     }
 }
